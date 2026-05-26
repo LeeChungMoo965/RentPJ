9826ac2f978fc2790eadd10dc7869fa46b6e2724
이충무 <cndan021@naver.com>
2026-05-26 14:12:50 +0900

    Admin 클래스 생성

---
+++ carrentproject/src/main/java/dto/Admin.java
@@ -0,0 +1,111 @@
+package dto;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Connection;
+import dao.ConnectionFactory;
+
+
+public class Admin extends Car{
+    private Connection conn;
+    private ArrayList<Car> listofCars = new ArrayList<Car>();
+
+    public Admin()  throws ClassNotFoundException, SQLException{
+        ConnectionFactory con= new ConnectionFactory();
+        con.setconnect();
+        this.conn = con.getconnect();
+    }
+   
+    public void addCar(Car car){
+        try{
+            String sql = new StringBuilder()
+            .append("INSERT INTO CAR (carid,carname,carprice,people,oil,color,distancy,endtime,locationX,locationY,filename,releasDate)")
+            .append(" VALUES (?,?,?,?,?,?,?,(TO_DATE(?,'YYYY-MM-DD HH24:MI')),?,?,?,SYSDATE)")
+            .toString();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, car.getCarid());
+            pstmt.setString(2, car.getCarname());
+            pstmt.setInt(3, car.getCarprice());
+            pstmt.setInt(4, car.getPeople());
+            pstmt.setString(5, car.getOil());
+            pstmt.setString(6, car.getColor());
+            pstmt.setInt(7, car.getDistancy());
+            pstmt.setString(8, car.getEndtime());
+            pstmt.setString(9, car.getLocationX());
+            pstmt.setString(10, car.getLocationY());
+            pstmt.setString(11, car.getFileName());
+            
+
+
+
+            
+            
+            int rows = pstmt.executeUpdate();
+            if(rows == 1){
+                System.out.println("추가 성공! ");
+                conn.commit();
+            }
+            pstmt.close();
+        }catch(SQLException e){
+            System.out.println(e);
+        }finally{
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                System.out.println(e);
+            }
+        }
+    }
+    public ArrayList<Car> getListofCars() {
+      
+        try{    
+            String sql = new StringBuilder()
+                .append("SELECT carid,carname,carprice,people,oil,color,distancy,locationX,locationY,filename,releasDate")
+                .append(" FROM CAR ")
+                .toString();
+                PreparedStatement pstmt = conn.prepareStatement(sql);            
+                ResultSet rs = pstmt.executeQuery();
+                while(true){
+                if(rs.next()){
+                    Car car = new Car();
+                    car.setCarid(rs.getString("carid"));
+                    car.setCarname(rs.getString("carname"));
+                    car.setPeople(rs.getInt("people"));
+                    car.setCarprice(rs.getInt("carprice"));
+                    car.setOil(rs.getString("oil"));
+                    car.setColor(rs.getString("color"));
+                    car.setFileName(rs.getString("filename"));
+                    car.setLocationX(rs.getString("locationX"));
+                    car.setLocationY(rs.getString("locationY"));
+                    car.setDistancy(rs.getInt("distancy"));
+                    listofCars.add(car);
+                }  else{
+                    break;
+                }
+
+            }
+            pstmt.close();
+        }catch(SQLException e){
+            System.out.println(e);
+        }
+        
+        return listofCars;
+    }
+    public Car getCarById(String carid){
+        Car carById = null;
+
+        
+
+        for( int i =0; i< listofCars.size(); i++){
+            Car car= listofCars.get(i);
+            if(car!=null && car.getCarid()!= null && car.getCarid().equals(carid)){
+                carById = car;
+                break;
+            } 
+
+        }
+        return carById;
+    }
+
+}
